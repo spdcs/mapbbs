eea4d29b756ffd964613f47ec7f65d94c6e25630
freee850516 <free19960516@gmail.com>
2016-08-25 16:38:21 +0800

    喵

--- index.php
+++ index.php
@@ -6,7 +6,6 @@ $username = $_SESSION['username'];
 $sql = "select bbs.id, data.username, data.sex, bbs.subject, bbs.time, bbs.content from bbs LEFT JOIN data ON data.account=bbs.account order by bbs.id desc";
 $result = mysql_query($sql);
 $row = mysql_fetch_row($result);
-
 ?>
 <html>
 <!DOCTYPE html>
@@ -48,14 +47,12 @@ $row = mysql_fetch_row($result);
 <div>
     <?php while ($row = mysql_fetch_row($result)): ?>
         <div class="well" >
-            <div class="login6"
         <br>第<?= $row[0] ?>位訪客
         <br>訪客姓名:<?= $row[1] ?>
         <br>性別:<?= $row[2] ?>生
         <br>留言主題:<?= $row[3] ?>
         <br>留言時間:<?= nl2br($row[4]) ?>
         <br>留言內容:<?= $row[5] ?>
-            </div>
         </div>
     <?php endwhile; ?>
 </div>
