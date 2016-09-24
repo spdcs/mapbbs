7dd326f94f23bfc80de24c266944b2975e58e0fd
freee850516 <free19960516@gmail.com>
2016-09-25 00:26:14 +0800

    邊框

--- bbsmap.php
+++ bbsmap.php
@@ -96,7 +96,7 @@ $row = mysql_fetch_row($result);
         }
 
     </script>
-    <div id="map" style="width: 800px; height: 600px"></div> <!--此為地圖顯示大小-->
+    <div id="map" style="width: 800px; height: 600px; border:double #dd1144"></div> <!--此為地圖顯示大小-->
     <script async defer
             src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA8hRHdZEOVwpfzjh_Yo5Pu0Aw_RrsOsT8&callback=initMap">
     </script>
